14c2f064c1e2afa703c88189504e72fb768f5b28
Johan Nilsson Hansen <granen@hotmail.com>
2015-06-22 23:09:00 +0200

    Added structs for directional/spot and point light

--- src/objects/lights/Lights.h
+++ src/objects/lights/Lights.h
@@ -7,13 +7,34 @@ using namespace chag;
 
 struct Light
 {
-	float3 color;
+	float3 ambientColor;
+	float3 diffuseColor;
+	float3 specularColor;
 
 	Light() {
-		color = make_vector(0.0f, 0.0f, 0.0f);
+		ambientColor  = make_vector(0.0f, 0.0f, 0.0f);
+		diffuseColor  = make_vector(0.0f, 0.0f, 0.0f);
+		specularColor = make_vector(0.0f, 0.0f, 0.0f);
 	}
 };
 
 struct DirectionalLight : public Light {
+	float3 direction;
+};
+
+struct PointLight : public Light {
 	float3 position;
+
+	struct {
+		float constant;
+		float linear;
+		float exp;
+	} attenuation;
+};
+
+struct SpotLight : public Light {
+	float3 direction;
+	float3 position;
+	float cutOff; //cos(rads) of the wanted angle 
+	float outerCutOff; //used for smooth edges
 };
