a4cea294e6b55fc9c8d6b99980a19a2e8f742aa1
Johan Nilsson Hansen <granen@hotmail.com>
2015-11-21 15:30:30 +0100

    testing travis

--- includes/IDrawable.h
+++ includes/IDrawable.h
@@ -7,7 +7,7 @@
 class IDrawable {
 public:
 	IDrawable() { shininess = 0.0f; };
-        virtual ~IDrawable()  = {};
+        virtual ~IDrawable()  {};
   virtual void render() = 0;
   virtual void renderShadow(Shader* shaderProgram) = 0;
   
