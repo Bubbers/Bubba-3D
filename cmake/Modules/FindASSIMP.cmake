a691c7a9931a357458b016ee1b98671bb04af840
Johan Nilsson Hansen <granen@hotmail.com>
2016-01-16 16:00:20 +0100

    Changed travis link for assimp

--- cmake/Modules/FindASSIMP.cmake
+++ cmake/Modules/FindASSIMP.cmake
@@ -34,7 +34,7 @@ FIND_PATH(ASSIMP_INCLUDE_DIR assimp/ai_assert.h
         /usr/local/include
         /usr/include/assimp
         /home/travis
-        /home/travis/build/Bubbad/Bubba-3D/lib
+        /home/travis/build/Bubbers/Bubba-3D/lib
         /usr/include
         /sw/include/assimp # Fink
         /sw/include
