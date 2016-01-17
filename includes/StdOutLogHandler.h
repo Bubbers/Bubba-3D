2a5760071bc7cd521ad31fc567f42496ab3d1eaa
Johan Nilsson Hansen <granen@hotmail.com>
2016-01-17 19:06:07 +0100

    Fixed stdOuthandler compileerror

--- includes/StdOutLogHandler.h
+++ includes/StdOutLogHandler.h
@@ -1,16 +1,17 @@
 
-#ifndef SUPER_BUBBA_AWESOME_SPACE_STDOUTLOGHANDLER_H
-#define SUPER_BUBBA_AWESOME_SPACE_STDOUTLOGHANDLER_H
+#ifndef SUPER_BUBBA_AWESOME_SPACE_STDOUTLOGHANDLER_H__
+#define SUPER_BUBBA_AWESOME_SPACE_STDOUTLOGHANDLER_H__
 
 #include "LogHandler.h"
+#include <string>
 
 class StdOutLogHandler : public LogHandler {
 public:
     StdOutLogHandler() {};
     ~StdOutLogHandler() {};
 
-    virtual void log(string msg) {
-        printf("%s", msg);
+    virtual void log(std::string msg) {
+        printf("%s", msg.c_str());
     };
 
 };
