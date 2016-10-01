90c02cba1db8c4dde08dc6b387d033c028030b21
Johan Nilsson Hansen <Granen@hotmail.com>
2016-10-01 20:49:55 +0200

    Added licence headers

--- includes/BoneInfo.h
+++ includes/BoneInfo.h
@@ -1,9 +1,20 @@
-//
-// Created by johan on 2016-10-01.
-//
-
-#ifndef BUBBAROGUEFORT_BONEINFO_H
-#define BUBBAROGUEFORT_BONEINFO_H
+/*
+ * This file is part of Bubba-3D.
+ *
+ * Bubba-3D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bubba-3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Bubba-3D. If not, see http://www.gnu.org/licenses/.
+ */
+#pragma once
 
 #include "linmath/float4x4.h"
 
@@ -13,5 +24,3 @@ struct BoneInfo
         chag::float4x4 finalTransformation;
     };
 
-
-#endif //BUBBAROGUEFORT_BONEINFO_H
